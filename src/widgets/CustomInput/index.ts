4e1018f46a0dd344d8f35f508e12b33ecc1b9988
daniel-williams <mail@danielwilliams.us>
2016-05-04 11:05:19 -0700

    Removing temp components used to test ngModel bindings.

--- src/widgets/CustomInput/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './custom-input.component';
-export * from './my-input.component';
-export * from './wrapped-input.accessor';
-export * from './wrapped-input.component';
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,2 +1,1 @@
 export * from './Countdown';
-export * from './CustomInput';
